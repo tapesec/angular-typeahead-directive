cadb7815508c5b64a26def970c1de313c3bb8608
tapesec <lionel.dupouy@gmail.com>
2014-12-31 15:38:57 +0100

    mise à jour

--- directives.js
+++ directives.js
@@ -1,21 +1,8 @@
 'use strict';
 
-/* Directives */
+angular.module('myApp.directives', [])
 
-
-
-var PermutantDirectives = angular.module('myApp.directives', []);
-
-
-PermutantDirectives.directive('appVersion', ['version', 
-	function(version) {
-    	return function(scope, elm, attrs) {
-      		elm.text(version);
-    	};
-  	}]);
-
-/*Typeahead*/
-PermutantDirectives.directive('autofill', function(){
+.directive('autofill', function(){
 	return {
 		restrict: 'E',
 		templateUrl: '../partials/directives-templates/typeahead.html',
@@ -62,66 +49,6 @@ PermutantDirectives.directive('autofill', function(){
 				ctrl.$setValidity(scope.inputname, true);
 				elem.children("div").children("ul").css('display', 'none');
 			}
-		}
-	}
-});
-
-PermutantDirectives.directive('confirmPassword', function($parse){
-	return{
-		restrict: 'A',
-		require: 'ngModel',
-		link: function(scope, elem, attrs, ctrl){
-			scope.$watch(attrs.ngModel, function(){
-				if(scope.$eval(attrs.ngModel) != scope.$eval(attrs.password)){
-					ctrl.$setValidity('like', false);
-				}else{
-					ctrl.$setValidity('like', true);
-				}
-			});
-			scope.$watch(attrs.password, function(){
-				if(scope.$eval(attrs.ngModel) != scope.$eval(attrs.password)){
-					ctrl.$setValidity('like', false);
-				}else{
-					ctrl.$setValidity('like', true);
-				}
-			});
-				
-
-		}
-	}
-});
-/*Directive qui détermine la validité d'un champ en fonction du boolean passé à l'attribut correct*/
-PermutantDirectives.directive('checkPassword', function(){
-	return {
-		restrict: 'A',
-		require: 'ngModel',
-		link: function(scope, elem, attrs, ctrl){
-			scope.$watch(attrs.correct, function(){
-				if(scope.$eval(attrs.correct) == false){
-					ctrl.$setValidity('wrongPassword', false);
-				}else{
-					ctrl.$setValidity('wrongPassword', true);
-				}
-			});	
-		}
-	}
-});
-
-
-/*Directives qui desactive un input si le select qui lui est raccroché n'a pas été utilisé*/
-PermutantDirectives.directive('multipleType', function(){
-
-	return {
-		restrict: 'A',
-		scope: {
-			select: "="
-		},
-		link: function(scope, elem, attrs, ctrl){
-			scope.$watch('select', function(nval){
-				if(nval != "pasDeCritere"){
-					$(elem).attr('disabled', false);
-				}
-			});
 		}
 	}
 });
